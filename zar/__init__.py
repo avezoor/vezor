6dec12af93f15fda461602d8bb0cc1f1095b0b81
avezoor <izzarsuly33@gmail.com>
2025-03-31 16:02:27 +0700

    31032025

---
+++ zar/__init__.py
@@ -0,0 +1,19 @@
+from .sorting import *
+from .searching import *
+from .geometry import *
+from . import geometry
+from . import searching
+from . import sorting
+
+__all__ = ["algoPrint", "algoWrite", "help"]
+__submodule__ = {
+    "sorting": sorting,
+    "searching": searching,
+    "geometry": geometry,
+    "formula": formula
+    }
+
+
+from .print import algoPrint
+from .write import algoWrite
+from .help import help
